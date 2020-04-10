27cd05f793e4614a9d192da2120a63fbd233e279
zibrulyaa <pzibrov@icloud.com>
2020-04-10 16:19:20 +0300

    added product-slider & product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function(){
-
-
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '13px',
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+    })
 });
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require("gulp"),
 
 gulp.task("sass", function() {
   return gulp
-    .src("app/scss/style.scss")
+    .src("app/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(
@@ -26,7 +26,8 @@ gulp.task("style", function() {
     .src([
       "node_modules/slick-carousel/slick/slick.css",
       'node_modules/normalize.css/normalize.css',
-      "node_modules/magnific-popup/dist/magnific-popup.css"
+      "node_modules/magnific-popup/dist/magnific-popup.css",
+      "node_modules/rateyo/src/jquery.rateyo.css"
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -36,7 +37,9 @@ gulp.task("script", function() {
   return gulp
     .src([
       "node_modules/slick-carousel/slick/slick.js",
-      "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
+      "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      "node_modules/mixitup/dist/mixitup.js",
+      "node_modules/rateyo/src/jquery.rateyo.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
@@ -56,7 +59,7 @@ gulp.task("browser-sync", function() {
   });
 });
 gulp.task("watch", function() {
-  gulp.watch("app/scss/style.scss", gulp.parallel("sass"));
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("app/*.html", gulp.parallel("html"));
   gulp.watch("app/js/*.js", gulp.parallel("js"));
 });
