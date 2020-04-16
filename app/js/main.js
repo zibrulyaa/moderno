0887c07d518cec4184c51d457b83c861462a87b3
zibrulyaa <pzibrov@icloud.com>
2020-04-16 20:20:15 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,28 @@
-$(function(){
-    var mixer = mixitup('.products__inner-box');
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: '13px',
-        readOnly: true
-    });
-    $('.product-slider__inner').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        arrows: false
-    })
+$(function () {
+  var mixer = mixitup(".products__inner-box");
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "13px",
+    readOnly: true,
+  });
+  $(".product-slider__inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false,
+  });
+});
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  grid: false,
+  prefix: "$",
+});
+$(".rate-star").rateYo({
+  rating: 5,
+  starWidth: "13px",
+  readOnly: true,
 });
