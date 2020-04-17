dbfac7547d68f0ded7542382238004371e32df7c
zibrulyaa <pzibrov@icloud.com>
2020-04-18 01:48:56 +0300

    added list style new items

--- app/js/main.js
+++ app/js/main.js
@@ -26,3 +26,13 @@ $(".rate-star").rateYo({
   starWidth: "13px",
   readOnly: true,
 });
+$(".icon-th-list").on("click", function () {
+  $(".products__item").addClass("list");
+  $(".icon-th-list").addClass("active");
+  $(".icon-th-large").removeClass("active");
+});
+$(".icon-th-large").on("click", function () {
+  $(".products__item").removeClass("list");
+  $(".icon-th-large").addClass("active");
+  $(".icon-th-list").removeClass("active");
+});
